b76e9b984983e484c9c3d1096bf5c297748785c6
Beman Dawes <bdawes@acm.org>
2002-12-30 19:14:06 +0000

    workaround VC++ 6 open problem
    [SVN r16722]

--- include/boost/filesystem/fstream.hpp
+++ include/boost/filesystem/fstream.hpp
@@ -27,12 +27,14 @@ namespace boost
     public:
       virtual ~basic_filebuf() {}
 
+#if !defined(BOOST_MSVC) || BOOST_MSVC > 1200 // VC++ 6.0 can't handle this
       std::basic_filebuf<charT,traits> * open( const path & file_ph,
         std::ios_base::openmode mode )
       {
         return std::basic_filebuf<charT,traits>::open(
           file_ph.native_file_string().c_str(), mode );
       }
+#endif
     };
 
     typedef basic_filebuf<char> filebuf;
@@ -50,12 +52,14 @@ namespace boost
         : std::basic_ifstream<charT,traits>(
         file_ph.native_file_string().c_str(), mode ) {}
       virtual ~basic_ifstream() {}
+#if !defined(BOOST_MSVC) || BOOST_MSVC > 1200 // VC++ 6.0 can't handle this
       void open( const path & file_ph,
         std::ios_base::openmode mode = std::ios_base::in )
       {
         std::basic_ifstream<charT,traits>::open(
           file_ph.native_file_string().c_str(), mode );
       }
+#endif
     };
 
     typedef basic_ifstream<char> ifstream;
@@ -73,12 +77,14 @@ namespace boost
         : std::basic_ofstream<charT,traits>(
         file_ph.native_file_string().c_str(), mode ) {}
       virtual ~basic_ofstream() {}
+#if !defined(BOOST_MSVC) || BOOST_MSVC > 1200 // VC++ 6.0 can't handle this
       void open( const path & file_ph,
         std::ios_base::openmode mode = std::ios_base::out )
       {
         std::basic_ofstream<charT,traits>::open(
           file_ph.native_file_string().c_str(), mode );
       }
+#endif
     };
 
     typedef basic_ofstream<char> ofstream;
@@ -96,12 +102,14 @@ namespace boost
         : std::basic_fstream<charT,traits>(
         file_ph.native_file_string().c_str(), mode ) {}
       virtual ~basic_fstream() {}
+#if !defined(BOOST_MSVC) || BOOST_MSVC > 1200 // VC++ 6.0 can't handle this
       void open( const path & file_ph,
         std::ios_base::openmode mode = std::ios_base::in|std::ios_base::out )
       {
         std::basic_fstream<charT,traits>::open(
           file_ph.native_file_string().c_str(), mode );
       }
+#endif
     };
  
     typedef basic_fstream<char> fstream;
