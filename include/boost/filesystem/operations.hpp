a0e91a1da42b04de8c73f6c5d3d63bfb889b90ac
Beman Dawes <bdawes@acm.org>
2003-11-30 13:48:05 +0000

    Clean up copyright, license, so inspect works
    [SVN r21015]

--- include/boost/filesystem/operations.hpp
+++ include/boost/filesystem/operations.hpp
@@ -1,14 +1,12 @@
 //  boost/filesystem/directory.hpp  ------------------------------------------//
 
-// < ----------------------------------------------------------------------- > 
-// <   Copyright © 2002, 2003 Beman Dawes.                                   >
-// <   Copyright © 2002 Jan Langer.                                          >
-// <   Copyright © 2001 Dietmar Kühl                                         >
-// <                                                                         > 
-// <   Use, modification, and distribution is subject to the Boost Software  >
-// <   License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy  >
-// <   at http://www.boost.org/LICENSE_1_0.txt)                              >
-// < ----------------------------------------------------------------------- > 
+//  Copyright © 2002, 2003 Beman Dawes
+//  Copyright © 2002 Jan Langer
+//  Copyright © 2001 Dietmar Kühl                                        
+//  
+//  Use, modification, and distribution is subject to the Boost Software
+//  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy
+//  at http://www.boost.org/LICENSE_1_0.txt)                             
 
 //  See library home page at http://www.boost.org/libs/filesystem
 
