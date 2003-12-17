3466847e720c44e09e5736ebddec4f827ca84dd6
Beman Dawes <bdawes@acm.org>
2003-12-17 01:37:58 +0000

    fix autolink problem
    [SVN r21295]

--- include/boost/filesystem/config.hpp
+++ include/boost/filesystem/config.hpp
@@ -41,7 +41,7 @@
 
 //  enable automatic library variant selection  ------------------------------// 
 
-#if !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_FILESYSTEM_NO_LIB)
+#if !defined(BOOST_FILESYSTEM_SOURCE) && !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_FILESYSTEM_NO_LIB)
 //
 // Set the name of our library, this will get undef'ed by auto_link.hpp
 // once it's done with it:
