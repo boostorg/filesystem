a3e185456cfae3ae6039a096617e9ef6e15ad7c0
Beman Dawes <bdawes@acm.org>
2005-05-14 13:07:23 +0000

    remove unneeded include
    [SVN r28895]

--- src/exception.cpp
+++ src/exception.cpp
@@ -20,7 +20,6 @@
 namespace fs = boost::filesystem;
 
 #include <cstring> // SGI MIPSpro compilers need this
-#include <string>
 
 # ifdef BOOST_NO_STDC_NAMESPACE
     namespace std { using ::strerror; }
