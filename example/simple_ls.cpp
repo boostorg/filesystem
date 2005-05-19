285b58f2a302abd65ef57b0807e78ad1cd9ad96c
Beman Dawes <bdawes@acm.org>
2005-05-19 12:19:05 +0000

    update for i18n
    [SVN r29058]

--- example/simple_ls.cpp
+++ example/simple_ls.cpp
@@ -17,7 +17,7 @@ namespace fs = boost::filesystem;
 int main( int argc, char* argv[] )
 {
 
-  fs::path full_path( fs::initial_path() );
+  fs::path full_path( fs::initial_path<fs::path>() );
 
   if ( argc > 1 )
     full_path = fs::system_complete( fs::path( argv[1], fs::native ) );
@@ -26,6 +26,7 @@ int main( int argc, char* argv[] )
 
   unsigned long file_count = 0;
   unsigned long dir_count = 0;
+  unsigned long other_count = 0;
   unsigned long err_count = 0;
 
   if ( !fs::exists( full_path ) )
@@ -48,13 +49,19 @@ int main( int argc, char* argv[] )
         if ( fs::is_directory( *dir_itr ) )
         {
           ++dir_count;
-          std::cout << dir_itr->leaf()<< " [directory]\n";
+          std::cout << dir_itr->leaf() << " [directory]\n";
         }
-        else
+        else if ( fs::is_file( *dir_itr ) )
         {
           ++file_count;
           std::cout << dir_itr->leaf() << "\n";
         }
+        else
+        {
+          ++other_count;
+          std::cout << dir_itr->leaf() << " [other]\n";
+        }
+
       }
       catch ( const std::exception & ex )
       {
@@ -64,6 +71,7 @@ int main( int argc, char* argv[] )
     }
     std::cout << "\n" << file_count << " files\n"
               << dir_count << " directories\n"
+              << other_count << " others\n"
               << err_count << " errors\n";
   }
   else // must be a file
