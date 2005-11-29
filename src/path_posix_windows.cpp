66bf10a50287c0878e24c8753e0b63106202ebb2
Beman Dawes <bdawes@acm.org>
2005-11-29 15:46:02 +0000

    bug 1151823, detect / in POSIX name
    [SVN r31822]

--- src/path_posix_windows.cpp
+++ src/path_posix_windows.cpp
@@ -139,9 +139,9 @@ namespace boost
       return windows_name( name );
     }
 #   else
-    BOOST_FILESYSTEM_DECL bool native( const std::string & )
+    BOOST_FILESYSTEM_DECL bool native( const std::string & name )
     {
-      return true;
+      return name.find('/') == std::string::npos;
     }
 #   endif
 
