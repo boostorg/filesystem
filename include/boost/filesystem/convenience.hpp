22bb191bf05a14829514401f7cf65c8af8edf119
Beman Dawes <bdawes@acm.org>
2006-03-09 22:15:07 +0000

    Remove copyright symbol as it causes VC++ warnings on Windows with codepage set to non-English, particularly Asian, languages
    [SVN r33295]

--- include/boost/filesystem/convenience.hpp
+++ include/boost/filesystem/convenience.hpp
@@ -1,7 +1,7 @@
 //  boost/filesystem/convenience.hpp  ----------------------------------------//
 
-//  © Copyright Beman Dawes, 2002-2005
-//  © Copyright Vladimir Prus, 2002
+//  Copyright Beman Dawes, 2002-2005
+//  Copyright Vladimir Prus, 2002
 //  Use, modification, and distribution is subject to the Boost Software
 //  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
