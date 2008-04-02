dcbe9649c263acbffd66d83e714bd0391a28a4a9
Marshall Clow <marshall@idio.com>
2008-04-02 01:42:32 +0000

    Replaced all occurrences of non-ASCII copyright symbol with '(c)' for people using non-ASCII code pages
    [SVN r43992]

--- example/mbpath.cpp
+++ example/mbpath.cpp
@@ -1,6 +1,6 @@
 //  Boost.Filesystem mbpath.hpp  ---------------------------------------------//
 
-//  © Copyright Beman Dawes 2005
+//  (c) Copyright Beman Dawes 2005
 
 //  Use, modification, and distribution is subject to the Boost Software
 //  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
