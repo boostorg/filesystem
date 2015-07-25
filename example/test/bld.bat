e9d72fa16cba59bcb3dfd2bc75c51fd1c40d3380
Beman <bdawes@acm.org>
2015-07-25 14:47:52 -0400

    Upgrade build infrastructure. Modernize tutorial examples to use C++11 to simplify code and make it easier to understand. Initial tutorial edits.

--- example/test/bld.bat
+++ example/test/bld.bat
@@ -3,5 +3,5 @@ rem Copyright Beman Dawes, 2010
 rem Distributed under the Boost Software License, Version 1.0.
 rem See www.boost.org/LICENSE_1_0.txt
 
-bjam %* >bjam.log
-find "error" <bjam.log
+bjam %* >b2.log
+find "error" <b2.log
--- example/test/bld.sh
+++ example/test/bld.sh
@@ -4,5 +4,5 @@
 # Distributed under the Boost Software License, Version 1.0.
 # See www.boost.org/LICENSE_1_0.txt
 
-bjam $* >bjam.log
-grep "error" <bjam.log
+b2 $* >b2.log
+grep "error" <b2.log
