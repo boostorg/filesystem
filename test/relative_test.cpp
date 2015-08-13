b54092e66f5156258f9a7dc89a52f9622e60b334
Beman <bdawes@acm.org>
2015-08-13 09:01:59 -0400

    Simplify expressions and remove accumulated cruft.

--- test/relative_test.cpp
+++ test/relative_test.cpp
@@ -14,28 +14,14 @@
 //  ----------------------------------------------------------------------------------  //
 
 #include <boost/config/warning_disable.hpp>
-
 #include <boost/filesystem/path.hpp>
-
 #include <boost/detail/lightweight_test_report.hpp>
-
 #include <iostream>
 
 namespace fs = boost::filesystem;
 using boost::filesystem::path;
 using std::cout;
 using std::endl;
-
-
-namespace
-{
-  boost::system::error_code ec;
-  const boost::system::error_code ok;
-  const boost::system::error_code ng(-1, boost::system::system_category());
-
-  std::string platform(BOOST_PLATFORM);
-
-}  // unnamed namespace
 
 //--------------------------------------------------------------------------------------//
 //                                                                                      //
@@ -58,47 +44,47 @@ int test_main(int, char*[])
   //  p == base / p.relative(base) otherwise return path(). However, if all elements of
   //  p are equal to the corresponding element of base, return path(".").
 
-  BOOST_TEST(path("").relative("") == path());
-  BOOST_TEST(path("").relative("/foo") == path());
-  BOOST_TEST(path("/foo").relative("") == path());
-  BOOST_TEST(path("/foo").relative("/foo") == path("."));
-  BOOST_TEST(path("").relative("foo") == path());
-  BOOST_TEST(path("foo").relative("") == path());
-  BOOST_TEST(path("foo").relative("foo") == path("."));
+  BOOST_TEST(path("").relative("") == "");
+  BOOST_TEST(path("").relative("/foo") == "");
+  BOOST_TEST(path("/foo").relative("") == "");
+  BOOST_TEST(path("/foo").relative("/foo") == ".");
+  BOOST_TEST(path("").relative("foo") == "");
+  BOOST_TEST(path("foo").relative("") == "");
+  BOOST_TEST(path("foo").relative("foo") == ".");
 
-  BOOST_TEST(path("a/b/c").relative("a") == path("b/c"));
-  BOOST_TEST(path("a/b/c").relative("a/b") == path("c"));
-  BOOST_TEST(path("a/b/c").relative("a/b/c") == path("."));
-  BOOST_TEST(path("a/b/c").relative("a/b/c/x") == path(".."));
-  BOOST_TEST(path("a/b/c").relative("a/b/c/x/y") == path("../.."));
-  BOOST_TEST(path("a/b/c").relative("a/x") == path("../b/c"));
-  BOOST_TEST(path("a/b/c").relative("a/b/x") == path("../c"));
-  BOOST_TEST(path("a/b/c").relative("a/x/y") == path("../../b/c"));
-  BOOST_TEST(path("a/b/c").relative("a/b/x/y") == path("../../c"));
-  BOOST_TEST(path("a/b/c").relative("a/b/c/x/y/z") == path("../../.."));
+  BOOST_TEST(path("a/b/c").relative("a") == "b/c");
+  BOOST_TEST(path("a/b/c").relative("a/b") == "c");
+  BOOST_TEST(path("a/b/c").relative("a/b/c") == ".");
+  BOOST_TEST(path("a/b/c").relative("a/b/c/x") == "..");
+  BOOST_TEST(path("a/b/c").relative("a/b/c/x/y") == "../..");
+  BOOST_TEST(path("a/b/c").relative("a/x") == "../b/c");
+  BOOST_TEST(path("a/b/c").relative("a/b/x") == "../c");
+  BOOST_TEST(path("a/b/c").relative("a/x/y") == "../../b/c");
+  BOOST_TEST(path("a/b/c").relative("a/b/x/y") == "../../c");
+  BOOST_TEST(path("a/b/c").relative("a/b/c/x/y/z") == "../../..");
  
-  BOOST_TEST(path("a/b/c").relative("x") == path());
-  BOOST_TEST(path("a/b/c").relative("x/y") == path());
-  BOOST_TEST(path("a/b/c").relative("x/y/z") == path());
-  BOOST_TEST(path("a/b/c").relative("x") == path());
-  BOOST_TEST(path("a/b/c").relative("x/y") == path());
-  BOOST_TEST(path("a/b/c").relative("x/y/z") == path());
+  BOOST_TEST(path("a/b/c").relative("x") == "");
+  BOOST_TEST(path("a/b/c").relative("x/y") == "");
+  BOOST_TEST(path("a/b/c").relative("x/y/z") == "");
+  BOOST_TEST(path("a/b/c").relative("x") == "");
+  BOOST_TEST(path("a/b/c").relative("x/y") == "");
+  BOOST_TEST(path("a/b/c").relative("x/y/z") == "");
   
   // TODO: add some Windows-only test cases that probe presence or absence of
   // drive specifier-and root-directory
 
   //  Some tests from Jamie Allsop's paper
-  BOOST_TEST(path("/a/d").relative("/a/b/c") == path("../../d"));
-  BOOST_TEST(path("/a/b/c").relative("/a/d") == path("../b/c"));
+  BOOST_TEST(path("/a/d").relative("/a/b/c") == "../../d");
+  BOOST_TEST(path("/a/b/c").relative("/a/d") == "../b/c");
 #ifdef BOOST_WINDOWS_API  
-  BOOST_TEST(path("c:\\y").relative("c:\\x") == path("../y"));
+  BOOST_TEST(path("c:\\y").relative("c:\\x") == "../y");
 #else
-  BOOST_TEST(path("c:\\y").relative("c:\\x") == path(""));
+  BOOST_TEST(path("c:\\y").relative("c:\\x") == "");
 #endif  
-  BOOST_TEST(path("d:\\y").relative("c:\\x") == path(""));
+  BOOST_TEST(path("d:\\y").relative("c:\\x") == "");
 
   //  From issue #1976
-  BOOST_TEST(path("/foo/new").relative("/foo/bar") == path("../new"));
+  BOOST_TEST(path("/foo/new").relative("/foo/bar") == "../new");
 
   return ::boost::report_errors();
 }
