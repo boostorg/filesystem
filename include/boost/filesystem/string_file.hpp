c026d612f251ab57a371ffa36629f806c3052af9
Beman <bdawes@acm.org>
2015-10-07 10:11:18 -0400

    String file feature cleanup: Add string_file.hpp to filesystem.hpp. Change string_file.hpp to use boost/filesystem/fstream.hpp for I/O. Add string_file_tests function to operations_unit_test.cpp.

--- include/boost/filesystem/string_file.hpp
+++ include/boost/filesystem/string_file.hpp
@@ -7,11 +7,11 @@
 
 //  Library home page: http://www.boost.org/libs/filesystem
 
-#ifndef BOOST_FILESYSTEM_FILE_SAVER_HPP
-#define BOOST_FILESYSTEM_FILE_SAVER_HPP
+#ifndef BOOST_FILESYSTEM_STRING_FILE_HPP
+#define BOOST_FILESYSTEM_STRING_FILE_HPP
 
-#include <fstream>
 #include <string>
+#include <boost/filesystem/fstream.hpp>
 #include <boost/filesystem/operations.hpp>
 
 namespace boost
@@ -19,21 +19,21 @@ namespace boost
 namespace filesystem
 {
 inline
-void save_string_file(const path& p, const std::string& contents)
+void save_string_file(const path& p, const std::string& str)
 {
-  std::ofstream file;
+  ofstream file;
   file.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-  file.open(p.string(), std::ios_base::binary);
-  file.write(contents.c_str(), contents.size());
+  file.open(p, std::ios_base::binary);
+  file.write(str.c_str(), str.size());
 }
 
 inline
 void load_string_file(const path& p, std::string& str)
 {
-  std::ifstream file;
+  ifstream file;
   file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  file.open(p.string(), std::ios_base::binary);
-  std::size_t sz = static_cast<std::size_t>(boost::filesystem::file_size(p));
+  file.open(p, std::ios_base::binary);
+  std::size_t sz = static_cast<std::size_t>(file_size(p));
   str.resize(sz, '\0');
   file.read(&str[0], sz);
 }
