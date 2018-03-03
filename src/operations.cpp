e5b37404403e5b4dad96c83fb5dbb2d0dd64efc1
Alexei Khlebnikov <alexei.khlebnikov@gmail.com>
2018-03-03 16:39:18 +0100

    Transformed _FILE_OFFSET_BITS #if-ery for better understanding.

--- src/operations.cpp
+++ src/operations.cpp
@@ -11,10 +11,15 @@
 //--------------------------------------------------------------------------------------// 
 
 //  define 64-bit offset macros BEFORE including boost/config.hpp (see ticket #5355) 
-#if !(defined(__HP_aCC) && defined(_ILP32) && !defined(_STATVFS_ACPP_PROBLEMS_FIXED))
+#if defined(__HP_aCC) && defined(_ILP32) && !defined(_STATVFS_ACPP_PROBLEMS_FIXED)
+// Do nothing.
+#else
 #define _FILE_OFFSET_BITS 64 // at worst, these defines may have no effect,
 #endif
-#if !defined(__PGI)
+
+#if defined(__PGI)
+#define _FILE_OFFSET_BITS 64
+#else
 #define __USE_FILE_OFFSET64 // but that is harmless on Windows and on POSIX
       // 64-bit systems or on 32-bit systems which don't have files larger 
       // than can be represented by a traditional POSIX/UNIX off_t type. 
@@ -25,8 +30,6 @@
       // ensure that they are available to all included headers.
       // That is required at least on Solaris, and possibly on other
       // systems as well.
-#else
-#define _FILE_OFFSET_BITS 64
 #endif
 
 // define BOOST_FILESYSTEM_SOURCE so that <boost/filesystem/config.hpp> knows
