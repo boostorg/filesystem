21494b5f98c07cfef42cc905bc4070252c6c7c15
Peter Dimov <pdimov@pdimov.com>
2018-09-17 23:16:42 +0300

    Do not convert to wstring on POSIX in unique_path

--- src/unique_path.cpp
+++ src/unique_path.cpp
@@ -130,17 +130,31 @@ namespace boost { namespace filesystem { namespace detail {
 BOOST_FILESYSTEM_DECL
 path unique_path(const path& model, system::error_code* ec)
 {
-  std::wstring s (model.wstring());  // std::string ng for MBCS encoded POSIX
+  // This function used wstring for fear of misidentifying
+  // a part of a multibyte character as a percent sign.
+  // However, double byte encodings only have 80-FF as lead
+  // bytes and 40-7F as trailing bytes, whereas % is 25.
+  // So, use string on POSIX and avoid conversions.
+
+  path::string_type s( model.native() );
+
+#ifdef BOOST_WINDOWS_API
   const wchar_t hex[] = L"0123456789abcdef";
+  const wchar_t percent = L'%';
+#else
+  const char hex[] = "0123456789abcdef";
+  const char percent = '%';
+#endif
+
   char ran[] = "123456789abcdef";  // init to avoid clang static analyzer message
                                    // see ticket #8954
   assert(sizeof(ran) == 16);
   const int max_nibbles = 2 * sizeof(ran);   // 4-bits per nibble
 
   int nibbles_used = max_nibbles;
-  for(std::wstring::size_type i=0; i < s.size(); ++i)
+  for(path::string_type::size_type i=0; i < s.size(); ++i)
   {
-    if (s[i] == L'%')                        // digit request
+    if (s[i] == percent)                     // digit request
     {
       if (nibbles_used == max_nibbles)
       {
