42f1ab9fb38a3b5b550b6dc3071a4db99c5a53d6
dsingh-mw <136297140+dsingh-mw@users.noreply.github.com>
2024-01-14 16:56:14 +0530

    179 skip fchmod flag (#1)
    * 179:Consider adding a flag for ignoring fchmod errors in copy_file

---
+++ test/issues/179.cpp
@@ -0,0 +1,68 @@
+#include <boost/filesystem.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <boost/filesystem/fstream.hpp> 
+#include <boost/system/system_category.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/system/system_error.hpp>
+#include <sys/types.h> // for mode_t type
+#include <sys/stat.h>
+#include <fstream>     // for std::ofstream
+#include <iostream>    // for std::cout
+
+namespace fs = boost::filesystem;
+using boost::system::error_code;
+using boost::system::system_category;
+typedef unsigned short mode_t;
+
+extern "C" int fchmod(int fd, mode_t mode) {
+    // Return -1 to indicate failure
+    return -1;
+}
+
+void create_file(const fs::path& ph, const std::string& contents = std::string())
+{
+    std::ofstream f(BOOST_FILESYSTEM_C_STR(ph));
+    if (!f.is_open())
+        throw fs::filesystem_error("operations_test create_file", ph, error_code(errno, system_category()));
+    if (!contents.empty())
+        f << contents;
+}
+
+void verify_file(const fs::path& ph, const std::string& expected)
+{
+    std::ifstream f(BOOST_FILESYSTEM_C_STR(ph));
+    if (!f)
+        throw fs::filesystem_error("operations_test verify_file", ph, error_code(errno, system_category()));
+    std::string contents;
+    f >> contents;
+    if (contents != expected)
+        throw fs::filesystem_error("operations_test verify_file contents \"" + contents + "\" != \"" + expected + "\"", ph, error_code());
+}
+
+int cpp_main(int argc, char* argv[])
+{
+    bool file_copied = false;
+    bool copy_ex_ok = true;
+    create_file("f1", "content"); // Ensure the source file exists with some content
+
+    try
+    {
+        // Attempt to use mocked fcmhod implementation which always errors out.
+        file_copied = fs::copy_file("f1", "f2", fs::copy_options::ignore_attribute_errors);
+    }
+    catch (const fs::filesystem_error&)
+    {
+        // If a filesystem_error is thrown, the option did not work as expected
+        copy_ex_ok = false;
+    }
+
+    BOOST_TEST(copy_ex_ok);
+    BOOST_TEST(file_copied);
+    BOOST_TEST(fs::exists("f2")); // The file should still exist despite the fchmod failure
+    verify_file("f2", "content");
+
+    fs::remove("f1");
+    fs::remove("f2");
+    return 0;
+}
