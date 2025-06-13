5a7e76d556a1b578e641dd44754c39204261fa10
Andrey Semashev <andrey.semashev@gmail.com>
2025-06-13 18:36:55 +0300

    Extend the dllimport workaround to any gcc 12.
    At least, gcc on Cygwin also emits the same bogus warnings about
    missing dllimport attributes. Presumably, this may also be the case
    for visibility attributes, so just disable the warning universally
    on any gcc 12.

--- include/boost/filesystem/directory.hpp
+++ include/boost/filesystem/directory.hpp
@@ -4,7 +4,7 @@
 //  Copyright Jan Langer 2002
 //  Copyright Dietmar Kuehl 2001
 //  Copyright Vladimir Prus 2002
-//  Copyright Andrey Semashev 2019, 2022
+//  Copyright Andrey Semashev 2019-2025
 
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
@@ -35,10 +35,10 @@
 
 #include <boost/filesystem/detail/header.hpp> // must be the last #include
 
-#if defined(__MINGW32__) && defined(BOOST_GCC) && (__GNUC__ == 12)
+#if defined(BOOST_GCC) && (__GNUC__ == 12)
 #pragma GCC diagnostic push
 // 'function' redeclared without dllimport attribute: previous dllimport ignored
-// MinGW-w64 bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106395
+// gcc bug on MinGW-w64 and Cygwin: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106395
 #pragma GCC diagnostic ignored "-Wattributes"
 #endif
 
@@ -1041,7 +1041,7 @@ struct range_const_iterator< boost::filesystem::recursive_directory_iterator, vo
 
 } // namespace boost
 
-#if defined(__MINGW32__) && defined(BOOST_GCC) && (__GNUC__ == 12)
+#if defined(BOOST_GCC) && (__GNUC__ == 12)
 #pragma GCC diagnostic pop
 #endif
 
